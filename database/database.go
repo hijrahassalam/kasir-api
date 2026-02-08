444fb71423f64286366073edf30ff71b5e9d55e3
Hijrah Assalam <31386955+hijrahassalam@users.noreply.github.com>
2026-02-08 19:25:04 +0700

    feat: add simple protocol mode to database connection string

--- database/database.go
+++ database/database.go
@@ -3,12 +3,22 @@ package database
 import (
 	"database/sql"
 	"log"
+	"strings"
 
 	// postgres driver - using pgx
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func InitDB(connectionString string) (*sql.DB, error) {
+	// Add simple protocol mode to avoid prepared statement cache issues
+	if !strings.Contains(connectionString, "default_query_exec_mode") {
+		if strings.Contains(connectionString, "?") {
+			connectionString += "&default_query_exec_mode=simple_protocol"
+		} else {
+			connectionString += "?default_query_exec_mode=simple_protocol"
+		}
+	}
+
 	// Open database
 	db, err := sql.Open("pgx", connectionString)
 	if err != nil {
